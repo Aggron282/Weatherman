91d6db9cd94b8d0941c623a3d76037991d5c0d5d
marcoking13 <marcokhodr16@gmail.com>
2024-07-12 14:35:11 -0700

    L

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var bcrypt = require("bcrypt");
+var session = require("express-session");
+var port = process.env.PORT || 3002;
+var app = express();
+var ejs = require("ejs");
+var weather_uri = require("./util/mongo_config.js").uri;
+var mongoose = require("mongoose");
+var main_router = require("./routes/main.js");
+
+app.set("views","views")
+app.set("view engine","ejs");
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(main_router);
+
+
+mongoose.connect(weather_uri).then((data_res)=>{
+  app.listen(port,()=>{
+    console.log("Weather App Running");
+  });
+})
